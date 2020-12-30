ce1e55b8465b33dbcba952684a36e664d64482a3
SofianAyayda <sofian_a@hotmail.fr>
2020-12-30 22:30:08 +0100

    Correction ajout () sur if

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from '@app/_services/authentication.service';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  email: string;
+  password: string;
+  error: string;
+  constructor(
+   private auth: AuthenticationService,
+   private router: Router
+   ) { }
+  imgLogo: string = "https://fast-it.fr/assets/logo_fastit.jpg";
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    this.auth.login(this.email, this.password)
+        .subscribe((res) => {
+        if (res === true) {
+          this.router.navigate(['startactivity'])
+          console.warn('test', res);
+          }
+        else {this.error = "Vous n'avez pas de profil Livreur"}
+        });
+  }
+
+}
